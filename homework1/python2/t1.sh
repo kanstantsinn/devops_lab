0306fc5ef9ce25826d16070e8e1e4ebe48fb7634
Kanstantsin Shchura <kanstantsin11@gmail.com>
2017-11-30 16:20:39 +0300

    improved script

--- homework1/python2/t1.sh
+++ homework1/python2/t1.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 sudo curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
  
-sudo echo "export PATH="/root/.pyenv/bin:$PATH"" >> $HOME/.bash_profile
-export PATH="/root/.pyenv/bin:$PATH"
-sudo yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel
-sudo yum install -y patch 
+echo "export PATH="/root/.pyenv/bin:$PATH"" >> $HOME/.bash_profile
+echo "export PATH="/root/.pyenv/bin:$PATH"" >> $HOME/.bashrc
+yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel patch
+source $HOME/.bash_profile
 pyenv install 2.7
 pyenv install 3.5.4
-#TMP_PATH=`echo $PATH`
-#sudo echo "export PATH="/root/.pyenv/versions/2.7/bin/:$PATH"" >> $HOME/.bash_profile
-#export PATH="/root/.pyenv/versions/2.7/bin/:$PATH"
-echo "eval "$(pyenv init -)"" >> $HOME/.bash_profile
-echo "eval "$(pyenv virtualenv-init -)"" >> $HOME/.bash_profile 
+echo 'eval "$(pyenv init -)"' >> $HOME/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bash_profile 
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
 source $HOME/.bash_profile
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-pyenv global 2.7
-pyenv virtualenv python2
-#sudo echo "export PATH="/root/.pyenv/versions/3.5.4/bin/:$TMP_PATH"" >> $HOME/.bash_profile
-#export PATH="/root/.pyenv/versions/3.5.4/bin/:$TMP_PATH"
-pyenv global 3.5.4
-pyenv virtualenv python3
-
-
+pyenv virtualenv 3.5.4 python3
+pyenv virtualenv 2.7 python2
+pyenv activate python2
 
 
